e18bd35e48327c8b28f3b4acd205d57cd4aa9035
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2026-01-08 11:28:14 -0300

    boton de ir para arriba btn top

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function cambiarIdioma(lang) {
     elemento.textContent = texto;
   });
 }
+const btnTop = document.getElementById("btnTop");
+
+// Mostrar/ocultar botón según scroll
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    btnTop.style.display = "block";
+  } else {
+    btnTop.style.display = "none";
+  }
+};
+
+// Scroll suave al inicio
+btnTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
